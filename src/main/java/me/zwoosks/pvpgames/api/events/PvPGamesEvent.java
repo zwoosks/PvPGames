8bee2fa86548d7037680437e6fbb615125899de3
zwoosks <zwoosks@protonmail.com>
2023-02-26 17:56:55 +0100

    Abstract PvpGamesEvent class to be able to complete the remaining events.

--- src/main/java/me/zwoosks/pvpgames/api/events/PvPGamesEvent.java
+++ src/main/java/me/zwoosks/pvpgames/api/events/PvPGamesEvent.java
@@ -1,4 +1,23 @@
 package me.zwoosks.pvpgames.api.events;
 
-public class PvPGamesEvent {
+import me.zwoosks.pvpgames.api.game.PvPGame;
+import org.bukkit.event.Event;
+
+public abstract class PvPGamesEvent extends Event {
+
+    protected PvPGame game;
+
+    public PvPGamesEvent(PvPGame game) {
+        this.game = game;
+    }
+
+    PvPGamesEvent(PvPGame game, boolean async) {
+        super(async);
+        this.game = game;
+    }
+
+    public final PvPGame getGame() {
+        return this.game;
+    }
+
 }
