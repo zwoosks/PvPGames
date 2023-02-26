4efb97cdd0451145936ac412e9af16e68b274a88
zwoosks <zwoosks@protonmail.com>
2023-02-26 14:25:15 +0100

    Add 2 GameType enums with different ScoreboardType values.

--- src/main/java/me/zwoosks/pvpgames/api/game/GameType.java
+++ src/main/java/me/zwoosks/pvpgames/api/game/GameType.java
@@ -4,20 +4,27 @@ import me.zwoosks.pvpgames.api.utils.ScoreboardType;
 
 public enum GameType {
 
-    TEST("Name", 60, ScoreboardType.BY_MINIGAME_SCORE);
+    FREE_FOR_ALL("Free For All", "Kill as many players as you can!", 60, ScoreboardType.BY_MINIGAME_SCORE),
+    PVP_DROPPER("PvP Dropper", "Kill them all. Be careful, there's holes in the floor!", 60, ScoreboardType.BY_GAME_SCORE);
 
     private String name;
+    private String description;
     private int gameDuration;
     private ScoreboardType scoreboardType;
 
-    GameType(String name, int gameDuration, ScoreboardType scoreboardType) {
+    GameType(String name, String description, int gameDuration, ScoreboardType scoreboardType) {
         this.name = name;
+        this.description = description;
         this.gameDuration = gameDuration;
         this.scoreboardType = scoreboardType;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
     }
 
     public int getGameDuration() {
