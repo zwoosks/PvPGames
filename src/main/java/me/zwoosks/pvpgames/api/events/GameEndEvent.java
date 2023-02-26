780f618733a6896681c5e7a137813059e54338db
zwoosks <zwoosks@protonmail.com>
2023-02-26 18:07:55 +0100

    Add code to GameEndEvent and GameStartEvent classes

--- src/main/java/me/zwoosks/pvpgames/api/events/GameEndEvent.java
+++ src/main/java/me/zwoosks/pvpgames/api/events/GameEndEvent.java
@@ -1,4 +1,47 @@
 package me.zwoosks.pvpgames.api.events;
 
-public class GameEndEvent {
+import me.zwoosks.pvpgames.api.game.PvPGame;
+import me.zwoosks.pvpgames.api.modules.CustomPlayer;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+
+public class GameEndEvent extends PvPGamesEvent {
+
+    private CustomPlayer first, second, third;
+    private CustomPlayer[] players;
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+
+    public GameEndEvent(PvPGame game, CustomPlayer first, CustomPlayer second, CustomPlayer third, CustomPlayer[] players) {
+        super(game);
+        this.first = first;
+        this.second = second;
+        this.third = third;
+        this.players = players;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+
+    public CustomPlayer[] getPlayers() {
+        return this.players;
+    }
+
+    public CustomPlayer getFirst() {
+        return this.first;
+    }
+
+    public CustomPlayer getSecond() {
+        return this.second;
+    }
+
+    public CustomPlayer getThird() {
+        return this.third;
+    }
+
 }
--- src/main/java/me/zwoosks/pvpgames/api/events/GameStartEvent.java
+++ src/main/java/me/zwoosks/pvpgames/api/events/GameStartEvent.java
@@ -1,4 +1,31 @@
 package me.zwoosks.pvpgames.api.events;
 
-public class GameStartEvent {
+import me.zwoosks.pvpgames.api.game.PvPGame;
+import me.zwoosks.pvpgames.api.modules.CustomPlayer;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+
+public class GameStartEvent extends PvPGamesEvent {
+
+    private CustomPlayer[] players;
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+
+    public GameStartEvent(PvPGame game, CustomPlayer[] players) {
+        super(game);
+        this.players = players;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+
+    public CustomPlayer[] getPlayers() {
+        return this.players;
+    }
+
 }
