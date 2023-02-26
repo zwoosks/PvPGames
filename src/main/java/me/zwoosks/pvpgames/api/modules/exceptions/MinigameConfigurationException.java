0af03f2659916eada3e087bf80cf693e91e74569
zwoosks <zwoosks@protonmail.com>
2023-02-26 14:31:57 +0100

    Custom exception in case config can't load for a minigame

---
+++ src/main/java/me/zwoosks/pvpgames/api/modules/exceptions/MinigameConfigurationException.java
@@ -0,0 +1,9 @@
+package me.zwoosks.pvpgames.api.modules.exceptions;
+
+public class MinigameConfigurationException extends Exception {
+
+    public MinigameConfigurationException(String message) {
+        super(message);
+    }
+
+}
