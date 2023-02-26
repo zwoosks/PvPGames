c189e3c1e735f47146a10ad1bd38879027803e54
zwoosks <zwoosks@protonmail.com>
2023-02-26 18:13:27 +0100

    Add LeaveGameEvent

--- src/main/java/me/zwoosks/pvpgames/api/events/LeaveGameEvent.java
+++ src/main/java/me/zwoosks/pvpgames/api/events/LeaveGameEvent.java
@@ -1,4 +1,33 @@
 package me.zwoosks.pvpgames.api.events;
 
-public class LeaveGameEvent {
+import me.zwoosks.pvpgames.api.game.LeaveGameReason;
+import me.zwoosks.pvpgames.api.game.PvPGame;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+
+public class LeaveGameEvent extends PvPGamesEvent {
+
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+    private Player who;
+    private final LeaveGameReason reason;
+
+    public LeaveGameEvent(PvPGame game, Player who, final LeaveGameReason reason) {
+        super(game);
+        this.who = who;
+        this.reason = reason;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+
+    public Player getPlayer() {
+        return this.who;
+    }
+
 }
