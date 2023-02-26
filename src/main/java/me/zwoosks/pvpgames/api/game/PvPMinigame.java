30b48c2448fdc5dd488a9f23dfb980443dc98e8d
zwoosks <zwoosks@protonmail.com>
2023-02-26 14:03:28 +0100

    PvPMinigame interface

--- src/main/java/me/zwoosks/pvpgames/api/game/PvPMinigame.java
+++ src/main/java/me/zwoosks/pvpgames/api/game/PvPMinigame.java
@@ -1,7 +1,28 @@
 package me.zwoosks.pvpgames.api.game;
 
+import me.zwoosks.pvpgames.api.modules.CustomPlayer;
+import org.bukkit.Location;
+
+import java.util.List;
+
 public interface PvPMinigame {
 
+    int getMinigameScore(CustomPlayer customPlayer);
 
+    PvPGame getGame();
+
+    GameState getGameState();
+
+    List<String> getScoreboardLines();
+
+    CustomPlayer[] getFinishedPlayers();
+
+    Location getMiddle();
+
+    Location[] getLocations();
+
+    int getElapsedTime();
+
+    int getAmountOfSpectators();
 
 }
